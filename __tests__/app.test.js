b262344be43a62a990a707718bf4bc92702d2533
indymoorcroft <indigo.moorcroft@hotmail.co.uk>
2024-08-23 16:18:30 +0100

    Task 2: GET /api completed

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -12,6 +12,23 @@ afterAll(() => {
   return db.end();
 });
 
+describe("GET: /api", () => {
+  test("200: responds with all the available endpoints of the api", () => {
+    return request(app)
+      .get("/api")
+      .expect(200)
+      .then(({ body: { endpoints } }) => {
+        expect(endpoints).toEqual(
+          expect.objectContaining({
+            "GET /api": expect.any(Object),
+            "GET /api/topics": expect.any(Object),
+            "GET /api/articles": expect.any(Object),
+          })
+        );
+      });
+  });
+});
+
 describe("GET: /api/topics", () => {
   test("200: responds with all of the topics", () => {
     return request(app)
--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+const { getEndpoints } = require("./controllers/endpoint-controller");
 const { getTopics } = require("./controllers/topic-controllers");
 
 const express = require("express");
 const app = express();
+
+app.get("/api", getEndpoints);
 
 app.get("/api/topics", getTopics);
 
