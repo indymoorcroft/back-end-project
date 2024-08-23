80f879a6dfe046fead9c1abc5956ca8283b3b4cc
indymoorcroft <indigo.moorcroft@hotmail.co.uk>
2024-08-23 17:06:42 +0100

    catch block added to getTopics controller

--- controllers/topic-controllers.js
+++ controllers/topic-controllers.js
@@ -1,7 +1,11 @@
 const { selectAllTopics } = require("../models/topic-models");
 
 exports.getTopics = (req, res, next) => {
-  selectAllTopics().then((topics) => {
-    res.status(200).send({ topics });
-  });
+  selectAllTopics()
+    .then((topics) => {
+      res.status(200).send({ topics });
+    })
+    .catch((err) => {
+      next(err);
+    });
 };
