25e4e840acdb12a1c15fa2d99cd95fb7c98b347d
indymoorcroft <indigo.moorcroft@hotmail.co.uk>
2024-08-27 15:11:38 +0100

    Task 6: GET /api/articles/:article_id/comments completed with testing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -105,12 +105,68 @@ describe("GET: /api/articles/:article_id", () => {
         expect(msg).toBe("Invalid Request");
       });
   });
-  test("400: response with an appropriate error message when given an invalid id", () => {
+  test("400: responds with an appropriate error message when given an invalid id", () => {
     return request(app)
       .get("/api/articles/not-an-id")
       .expect(400)
       .then(({ body: { msg } }) => {
         expect(msg).toBe("Bad Request");
+      });
+  });
+});
+
+describe("GET: /api/articles/:article_id/comments", () => {
+  test("200: responds with an array of comments for the given article_id", () => {
+    return request(app)
+      .get("/api/articles/9/comments")
+      .expect(200)
+      .then(({ body: { comments } }) => {
+        expect(comments).toHaveLength(2);
+        comments.forEach((comment) => {
+          expect(comment).toEqual(
+            expect.objectContaining({
+              comment_id: expect.any(Number),
+              body: expect.any(String),
+              votes: expect.any(Number),
+              author: expect.any(String),
+              article_id: expect.any(Number),
+              created_at: expect.any(String),
+            })
+          );
+        });
+      });
+  });
+  test("404: sends an appropriate status and error message when given a valid but non-existent id", () => {
+    return request(app)
+      .get("/api/articles/999/comments")
+      .expect(404)
+      .then(({ body: { msg } }) => {
+        expect(msg).toBe("Invalid Request");
+      });
+  });
+  test("400: responds with an appropriate error message when given an invalid id", () => {
+    return request(app)
+      .get("/api/articles/not-an-id/comments")
+      .expect(400)
+      .then(({ body: { msg } }) => {
+        expect(msg).toBe("Bad Request");
+      });
+  });
+  test("200: article_id exists by has no related comments, responds with an empty array", () => {
+    return request(app)
+      .get("/api/articles/2/comments")
+      .expect(200)
+      .then(({ body: { comments } }) => {
+        expect(comments).toHaveLength(0);
+        expect(Array.isArray(comments)).toBe(true);
+      });
+  });
+  test("200: responds with all of the comments sorted by created_at in descending order", () => {
+    return request(app)
+      .get("/api/articles/1/comments")
+      .expect(200)
+      .then(({ body: { comments } }) => {
+        expect(comments).toBeSortedBy("created_at", { descending: true });
       });
   });
 });
--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const {
   getArticles,
   getArticleById,
 } = require("./controllers/article-controllers");
+const { getCommentsById } = require("./controllers/comment-controllers");
 
 const express = require("express");
 const app = express();
@@ -15,6 +16,8 @@ app.get("/api/topics", getTopics);
 app.get("/api/articles", getArticles);
 
 app.get("/api/articles/:article_id", getArticleById);
+
+app.get("/api/articles/:article_id/comments", getCommentsById);
 
 app.use((err, req, res, next) => {
   if (err.code === "22P02") {
