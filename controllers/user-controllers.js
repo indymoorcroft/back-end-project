ff77b26f65e6eae925c6e135cde0be9663e5ee47
indymoorcroft <indigo.moorcroft@hotmail.co.uk>
2024-08-29 14:07:14 +0100

    Task 17 GET /api/users/:username

--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -1,9 +1,23 @@
-const { selectAllUsers } = require("../models/user-models");
+const {
+  selectAllUsers,
+  selectUserByUsername,
+} = require("../models/user-models");
 
 exports.getUsers = (req, res, next) => {
   selectAllUsers()
     .then((users) => {
       res.status(200).send({ users });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+
+exports.getUserByUsername = (req, res, next) => {
+  const { username } = req.params;
+  selectUserByUsername(username)
+    .then((user) => {
+      res.status(200).send({ user });
     })
     .catch((err) => {
       next(err);
--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,8 +1,13 @@
-const { getUsers } = require("../controllers/user-controllers");
+const {
+  getUsers,
+  getUserByUsername,
+} = require("../controllers/user-controllers");
 
 const express = require("express");
 const userRouter = express.Router();
 
 userRouter.get("/api/users", getUsers);
+
+userRouter.get("/api/users/:username", getUserByUsername);
 
 module.exports = userRouter;
